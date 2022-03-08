ba21da64dbd8e5f8baf2fc53279841f5c963ee25
Pavlaso <pavlaso25@mail.ru>
2022-03-08 20:01:22 +0300

    add procfile

--- src/api/sushi.ts
+++ src/api/sushi.ts
@@ -3,4 +3,4 @@ import { ISushi } from "../types/types";
 
 export const getSushiAPI = (category: any, filters: any) => {
     return axios.get<ISushi[]>(`http://localhost:3001/sushi?${category ==='Все' ? '' :`category=${category}`}&_sort=${filters}`).then(res => res.data)
-}
+} 
