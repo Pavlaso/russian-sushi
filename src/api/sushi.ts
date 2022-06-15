1e045667c66fa02f2ffe938249ead10e21e1abcd
Pavlaso <pavlaso25@mail.ru>
2022-06-15 18:44:16 +0300

    change ports

--- src/api/sushi.ts
+++ src/api/sushi.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 import { ISushi } from "../types/types";
 
 export const getSushiAPI = (category: any, filters: any) => {
-    return axios.get<ISushi[]>(`http://localhost:3001/sushi?${category ==='Все' ? '' :`category=${category}`}&_sort=${filters}`).then(res => res.data)
+    return axios.get<ISushi[]>(`/sushi?${category ==='Все' ? '' :`category=${category}`}&_sort=${filters}`).then(res => res.data)
 } 
